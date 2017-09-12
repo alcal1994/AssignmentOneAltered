242e99f418efe453c2c7f15dab14af3f4a3c80fe
JaretWright <hkkA223>
2017-09-12 06:15:41 -0400

    added GUI elements

---
+++ src/time/ClockController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author JWright
+ */
+public class ClockController implements Initializable {
+
+    @FXML private TextField hoursTextField;
+    @FXML private TextField minutesTextField;
+    @FXML private TextField secondsTextField;
+    
+    @FXML private Button updateTimeButton;
+    @FXML private Label miltaryTimeLabel;
+    @FXML private Label standardTimeLabel;
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
